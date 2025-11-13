32cb1b6589ef15662a18f4dcab7db6ace36d0db8
Luigi Leonardi <leonardi@redhat.com>
2025-11-13 08:09:08 -0500

    kernel: add vsock layer API
    Add a simple vsock layer API trait.
    
    Signed-off-by: Luigi Leonardi <leonardi@redhat.com>

---
+++ kernel/src/vsock/mod.rs
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+//
+// Copyright (c) 2025 Red Hat, Inc.
+//
+// Author: Luigi Leonardi <leonardi@redhat.com>
+
+pub mod api;
+pub mod error;
+
+pub use error::VsockError;
+
+extern crate alloc;
+use crate::{utils::immut_after_init::ImmutAfterInitCell, vsock::api::VsockDriver};
+use alloc::boxed::Box;
+
+static VSOCK_DEVICE: ImmutAfterInitCell<Box<dyn VsockDriver>> = ImmutAfterInitCell::uninit();
