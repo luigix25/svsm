luigix25/svsm

commit a097c1ddcb367db4b9c64b050e39e0a13c3e0edc

xbuild: use packit as a library
As of right now, we build packit as a binary from xbuild in order to
create the filesystem image to be embedded into the IGVM file. However,
a recently change in packit simplifies using it as a Rust library, so
use it as such to better integrate it into the codebase.

This unfortunately introduces issues with workspace-level feature
unification, causing in some cases for packit to be linked with the
combination of all of its features used in the workspace, which includes
the "std" feature that xbuild needs, causing issues for the SVSM kernel.
For now the only workaround in cargo is to use the `--package` option
and adding `--exclude svsm` when using `--workspace`.

On the other hand, this lets us better specify that SVSM_ARGS_TEST in
the Makefile should only be passed to the SVSM kernel target, and not
the rest.

Signed-off-by: Carlos López <clopez@suse.de>

Makefile
@@ -116,11 +116,14 @@ bin/coconut-test-vanadium.igvm:
 	cargo xbuild $(XBUILD_ARGS_TEST) ./configs/test/vanadium-test-target.json
 
 test:
-	cargo test ${CARGO_ARGS} ${SVSM_ARGS_TEST} --workspace
+	cargo test ${CARGO_ARGS} ${SVSM_ARGS_TEST} --package svsm
+	cargo test ${CARGO_ARGS} --workspace --exclude svsm
 
 miri:
 	MIRIFLAGS=-Zmiri-permissive-provenance \
-		cargo +nightly miri test ${CARGO_ARGS} ${SVSM_ARGS_TEST} --workspace
+		cargo +nightly miri test ${CARGO_ARGS} ${SVSM_ARGS_TEST} --package svsm
+	MIRIFLAGS=-Zmiri-permissive-provenance \
+		cargo +nightly miri test ${CARGO_ARGS} --workspace --exclude svsm
 
 test-igvm: $(IGVM_TEST_FILES)
 
@@ -176,7 +179,8 @@ clippy:
 	RUSTFLAGS="--cfg fuzzing" ${CARGO} clippy ${CLIPPY_OPTIONS} --package svsm-fuzz -- ${CLIPPY_ARGS}
 	${CARGO} clippy ${CLIPPY_OPTIONS} --package svsm --target x86_64-unknown-none -- ${CLIPPY_ARGS}
 	${CARGO} clippy ${CLIPPY_OPTIONS} --package stage1 --target x86_64-unknown-none -- ${CLIPPY_ARGS} ${STAGE1_RUSTC_ARGS}
-	${CARGO} clippy ${CLIPPY_OPTIONS} --workspace --tests --exclude packit -- ${CLIPPY_ARGS}
+	${CARGO} clippy ${CLIPPY_OPTIONS} --workspace --tests --exclude svsm -- ${CLIPPY_ARGS}
+	${CARGO} clippy ${CLIPPY_OPTIONS} --package svsm ${SVSM_ARGS_TEST} --tests -- ${CLIPPY_ARGS}
 
 clean:
 	cargo clean

xbuild/Cargo.toml
@@ -5,6 +5,7 @@ edition.workspace = true
 
 [dependencies]
 clap = { workspace = true, features = ["color", "derive", "help", "std", "suggestions", "usage"] }
+packit = { workspace = true, features = ["std"] }
 serde = { workspace = true, features = ["std", "derive"] }
 serde_json = { workspace = true, default-features = true }
 

xbuild/src/fs.rs
@@ -2,14 +2,11 @@
 //
 // Author: Carlos López <carlos.lopezr4096@gmail.com>
 
-use crate::{
-    Args, BuildResult, BuildTarget, Component, ComponentConfig, features::Features,
-    helpers::HELPERS, run_cmd_checked,
-};
+use crate::{Args, BuildResult, BuildTarget, Component, ComponentConfig, features::Features};
+use packit::PackItArchiveEncoder;
 use serde::Deserialize;
 use std::collections::HashMap;
 use std::path::{Path, PathBuf};
-use std::process::Command;
 
 /// Components for the filesystem image.
 #[derive(Debug, Clone, Deserialize)]
@@ -42,7 +39,11 @@ impl FsConfig {
             return Ok(None);
         }
 
-        // Build all components and copy them to the output path
+        let fs_path = PathBuf::from("bin/svsm-fs.bin");
+        let mut fs = std::fs::File::create(&fs_path)?;
+        let mut enc = PackItArchiveEncoder::new(&mut fs)?;
+
+        // Build all components and add them to the archive
         for comp in self.components() {
             let bin = comp.build(args, BuildTarget::svsm_user(), cmd_feats)?;
             let mut dst_file = comp
@@ -57,19 +58,14 @@ impl FsConfig {
             }
             dst.push(dst_file);
             comp.config.objcopy.copy(&bin, &dst, args)?;
+
+            let filename = dst_file
+                .to_str()
+                .ok_or_else(|| format!("invalid file name: {}", dst_file.display()))?;
+            enc.load_file(filename, &std::fs::File::open(&dst)?)?;
             dst.pop();
         }
 
-        // Now build filesystem image from all components
-        let fs = PathBuf::from("bin/svsm-fs.bin");
-        let mut cmd = Command::new(HELPERS.packit(args, cmd_feats));
-        cmd.arg("pack")
-            .arg("--input")
-            .arg(&dst)
-            .arg("--output")
-            .arg(&fs);
-        run_cmd_checked(cmd, args)?;
-
-        Ok(Some(fs))
+        Ok(Some(fs_path))
     }
 }

xbuild/src/helpers.rs
@@ -2,7 +2,7 @@
 //
 // Author: Carlos López <carlos.lopezr4096@gmail.com>
 
-use crate::{Args, BuildTarget, Component, ComponentConfig, features::Features};
+use crate::{Args, BuildTarget, Component, features::Features};
 use std::path::PathBuf;
 use std::sync::OnceLock;
 
@@ -11,7 +11,6 @@ use std::sync::OnceLock;
 pub struct Helpers {
     igvmbuilder: OnceLock<PathBuf>,
     igvmmeasure: OnceLock<PathBuf>,
-    packit: OnceLock<PathBuf>,
 }
 
 impl Helpers {
@@ -19,7 +18,6 @@ impl Helpers {
         Self {
             igvmbuilder: OnceLock::new(),
             igvmmeasure: OnceLock::new(),
-            packit: OnceLock::new(),
         }
     }
 
@@ -36,20 +34,6 @@ impl Helpers {
             Component::new_default("igvmmeasure")
                 .build(args, BuildTarget::Host, cmd_feats)
                 .expect("failed to build igvmmeasure")
-        })
-    }
-
-    pub fn packit(&self, args: &Args, cmd_feats: &mut Features) -> &PathBuf {
-        self.packit.get_or_init(|| {
-            Component::new(
-                "packit",
-                ComponentConfig {
-                    features: Some("cli".into()),
-                    ..Default::default()
-                },
-            )
-            .build(args, BuildTarget::Host, cmd_feats)
-            .expect("failed to build packit")
         })
     }
 }

commit 5cce40d30f3238f6bdb82a361f2dbcc86e55bf52

Makefile: fix clippy with CARGO_HACK
Running `make clippy CARGO_HACK=1` fails with:

    error: --no-default-features may not be used together with --each-feature

The `SVSM_ARGS_TEST` variable is only used to run tests. No need to use
it in clippy.

Fixes: a097c1dd ("xbuild: use packit as a library")
Suggested-by: Stefano Garzarella <sgarzare@redhat.com>
Signed-off-by: Luigi Leonardi <leonardi@redhat.com>

Makefile
@@ -180,7 +180,7 @@ clippy:
 	${CARGO} clippy ${CLIPPY_OPTIONS} --package svsm --target x86_64-unknown-none -- ${CLIPPY_ARGS}
 	${CARGO} clippy ${CLIPPY_OPTIONS} --package stage1 --target x86_64-unknown-none -- ${CLIPPY_ARGS} ${STAGE1_RUSTC_ARGS}
 	${CARGO} clippy ${CLIPPY_OPTIONS} --workspace --tests --exclude svsm -- ${CLIPPY_ARGS}
-	${CARGO} clippy ${CLIPPY_OPTIONS} --package svsm ${SVSM_ARGS_TEST} --tests -- ${CLIPPY_ARGS}
+	${CARGO} clippy ${CLIPPY_OPTIONS} --package svsm --tests -- ${CLIPPY_ARGS}
 
 clean:
 	cargo clean